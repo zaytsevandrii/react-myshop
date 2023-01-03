62ede6b5572a668bb236406afc170207d383b4b0
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-03 12:57:52 +0000

    add header,cart and create redux-store

--- react-myshop/src/App.js
+++ react-myshop/src/App.js
@@ -1,10 +1,33 @@
+import { useEffect } from "react"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import Checkout from "./pages/checkout/Checkout"
+import Confirmation from "./pages/checkout/Confirmation"
+import Navbar from "./components/Global/Navbar"
+import Home from "./pages/Home/Home"
+import ItemDetails from "./pages/itemDetails/ItemDetails"
+import CartMenu from "./components/Global/CartMenu"
 
+const ScrollToTop = () => {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+}
 function App() {
-  return (
-    <div className="App">
-     dsd
-    </div>
-  );
+    return (
+        <BrowserRouter>
+        <Navbar/>
+            <ScrollToTop />
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/item/:id' element={<ItemDetails/>}/>
+              <Route path='/checkout' element={<Checkout/>}/>
+              <Route path='/checkout/success' element={<Confirmation/>}/>
+            </Routes>
+            <CartMenu/>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
